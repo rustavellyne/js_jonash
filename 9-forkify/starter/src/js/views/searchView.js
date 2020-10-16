68a5b1c94edbedb644501f8783b332a25e323bc6
rustavellyne <rustavellyne@gmail.com>
2020-10-16 19:35:27 +0300

    ada

--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -10,6 +10,20 @@ export const clearResults = () => {
 	elements.searchResultList.innerHtml = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+	const newTitle = [];
+	if(title.length > limit) {
+		title.split(' ').reduce((acc, curr) => {
+			if (acc + curr.length < limit) {
+				newTitle.push(curr)
+			}
+			return acc + curr.length
+		}, 0)
+		return `${newTitle.join(' ')}...`;
+	}
+	return title;
+}
+
 const renderRecipe = recipe => {
 	const template = `
 		<li>
@@ -18,7 +32,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name sdasd">${recipe.title}</h4>
+                    <h4 class="results__name sdasd">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
